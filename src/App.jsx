769126a92e4a5a3316347d0ec9765f9b35dfd5f4
Owen <owentan@tac-dynamics.com>
2025-06-11 10:23:00 +0800

    update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,72 @@
 import { useState } from 'react'
 import './App.css'
 import Alert from '@mui/material/Alert';
-import { TextField } from '@mui/material';
+import { TextField, Paper, Box, Button, Stack, Divider,Card } from '@mui/material';
+import { green } from '@mui/material/colors';
 
 function App() {
   const [result, setResult] = useState(null);
+  const [moveTime, setMoveTime] = useState(0);
+  const [trfTime, setTrfTime] = useState(0);
+  const [climbTime, setClimbtime] = useState(0);
 
   function calculate(formData) {
     if ((formData.get("length")) != '' && formData.get("breadth") != '' && formData.get("height") != '') {
       const length = Number(formData.get("length"));
       const breadth = Number(formData.get("breadth"));
       const height = Number(formData.get("height"));
+
+      const move_t = Number(formData.get("move"));
+      const trf_t = Number(formData.get("transform"));
+      const climb_t = Number(formData.get("climb"));
+      const turn_t = Number(formData.get("turn"))+ 1.5; //需要加1.5s 
+
+      const x = Number(formData.get("x"));
+      const y = Number(formData.get("y"));
+      const z = Number(formData.get("z"));
+
       const moves = 1 + length + breadth + height;
-      setResult(<Alert variant="filled" severity="success">Total moves: {moves}</Alert>);
+      //const totaltime = moves*move_t + 
+      setResult(<Paper elevation={0} style={{backgroundColor: "lightgreen",padding:'100px'}}> 
+      Total moves: {moves}
+      Total moves: {moves}
+      </Paper>);
     } else {
-      setResult(<Alert severity="error">請輸入相關資料</Alert>);
+      setResult(<Paper backgroundColor={"pink"}>請輸入相關資料</Paper>);
     }
   }
   return (
     <form action={calculate}>
-      <div style={{
-        backgroundColor: "#F0F8FF", borderColor: "#fcfdfb",
-        borderWidth: 2, padding: 20, display: 'flex', gap: 20, width: "500px"
-      }}>
-        <div style={{ width: '100%' }}>
-          Length 长度: <TextField  name="length" /><br />
-          Breadth 宽度: <TextField  name="breadth" /><br />
-          Height 高度: <TextField  name="height" /><br />
-        </div>
-        <div style={{ width: '100%' }}>
-          Moving time (s): <TextField  name="move" /><br />
-          Transform time (s): <TextField  name="transform" /><br />
-          Climbing time (s): <TextField  name="climb" /><br />
-        </div>
-
-      </div>
-      <button type="submit" style={{ marginTop: '10px' }}>Calculate</button>
-      {result !== null && <div style={{ marginTop: '10px' }}> {result}</div>}
+      <Stack gap={1} maxWidth={1500} height={350} width={"100%"} minWidth={400} direction={"row"} alignContent={'center'}>
+        <Paper elevation={0} style={{
+          backgroundColor: "white", borderColor: "#fcfdfb",
+          borderWidth: 2, padding: 20, width: '100%', border: '1px solid #ccc'
+        }}>
+          <Box display={'flex'} gap={4} flexDirection={'column'}>
+            <Box display="flex" gap={1} width="100%" flexDirection={'row'} alignItems={'center'}>
+              Length 长度: <TextField width={10} type="number" name="length" sx={{ flex: 1, width:"100px"}}/>
+              Breadth 宽度: <TextField type="number" name="breadth" sx={{ flex: 1, width:"100px" }} />
+              Height 高度: <TextField type="number" name="height"sx={{ flex: 1, width:"100px" }} />
+            </Box>
+            <Box display="flex" gap={1} width="100%" flexDirection={'row'} alignItems={'center'}>
+              移動時間 (s): <TextField name="move" sx={{ flex: 1, width:"100px"}} />
+              轉向時間 (s): <TextField name="transform"sx={{ flex: 1, width:"100px" }} />
+              爬升時間 (s): <TextField name="climb" sx={{ flex: 1, width:"100px" }}/>
+              轉盤時間 (s): <TextField name="rotate" sx={{ flex: 1, width:"100px" }}/>
+            </Box>
+            <Divider orientation="horizontal" flexItem />
+            <Box display="flex" gap={1} width="100%" flexDirection={'row'} alignItems={'center'}>
+              x: <TextField name="x" sx={{ flex: 1, width:"100px" }} />
+              y: <TextField name="y" sx={{ flex: 1, width:"100px"}} />
+              z: <TextField name="z" sx={{ flex: 1, width:"100px" }} />
+            </Box>
+          </Box>
+          <Button type="submit" variant="outlined" style={{ marginTop: '10px' }}>Calculate</Button>
+        </Paper>
+        {result !== null && <Box style={{ height:"100%", maxwidth: 100, width: '100%'}}> {result}</Box>}
+      </Stack>
     </form>
-
   );
-
-  return (
-    <>
-      <div>
-
-      </div>
-      <h1>TPH Calculator</h1>
-      <form className="card">
-        <input name='length'></input>
-        <input></input>
-        <button type="submit">
-          Calculate
-        </button>
-      </form>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
 
 export default App
